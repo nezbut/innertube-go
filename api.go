b047d0a8e4f61c852e8e6df372122c66a290cce6
nezbut <anonim@anonim.com>
2025-03-22 23:08:09 +0700

    fix: fix warning

--- api.go
+++ api.go
@@ -4,7 +4,7 @@ import "strings"
 
 func GetContext(clientName string) ClientContext {
 	for _, clientContext := range config.Clients {
-		if strings.ToUpper(clientContext.ClientName) == strings.ToUpper(clientName) {
+		if strings.EqualFold(strings.ToUpper(clientContext.ClientName), strings.ToUpper(clientName)) {
 			return clientContext
 		}
 	}
